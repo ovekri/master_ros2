f8c2f48f02456fb39880f620db3c5739ac6c876d
ovekri <ovekn@outlook.com>
2024-04-13 20:19:19 +0200

    converted /camera/depth/image_rec_raw to pointcloud2 topic

---
+++ install/launch_car/share/launch_car/launch/realsense_launch.py
@@ -0,0 +1,30 @@
+import os
+import launch
+import launch_ros.actions
+from launch.substitutions import Command
+from launch.actions import IncludeLaunchDescription
+from ament_index_python import get_package_share_directory
+from launch_ros.parameter_descriptions import ParameterValue
+from ament_index_python.packages import get_package_share_path
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+
+def generate_launch_description():
+    robot_description = ParameterValue(
+        Command(['xacro ', str(get_package_share_path('realsense2_description') / 'urdf/test_d435_camera.urdf.xacro')]),
+        value_type=str)
+
+    rs_cam = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('realsense2_camera'), 'launch/rs_launch.py')),
+        launch_arguments={
+            "depth_module.profile": "640x480x30",
+           # "pointcloud.enable": "True",
+        }.items()
+    )
+
+    return launch.LaunchDescription([
+        rs_cam,
+        launch_ros.actions.Node(package='robot_state_publisher', executable='robot_state_publisher',
+                                parameters=[{'robot_description': robot_description}])
+    ])
---
+++ src/launch_ws/src/launch_car/launch/raw_to_PC2.py
@@ -0,0 +1,31 @@
+import launch
+from launch_ros.descriptions import ComposableNode
+from launch_ros.actions import ComposableNodeContainer
+
+def generate_launch_description():
+    """Generate launch description for converting Realsense data to PointCloud2."""
+    container = ComposableNodeContainer(
+        name='point_cloud_converter',
+        namespace='',
+        package='rclcpp_components',
+        executable='component_container',
+        composable_node_descriptions=[
+            ComposableNode(
+                package='rtabmap_util',
+                plugin='rtabmap_util::PointCloudXYZ',
+                name='points_xyz_rt',
+                remappings=[
+                            ("depth/image", "/camera/depth/image_rect_raw"),
+                            ("depth/camera_info", "/camera/depth/camera_info"),
+                            ("cloud", "/camera/depth/color/points")
+                            ],
+                parameters=[
+                            {"decimation": 4},
+                            {"voxel_size": 0.05},
+                            {"approx_sync": False}
+                            ]
+                ),
+        ],
+        output='screen',
+    )
+    return launch.LaunchDescription([container])
